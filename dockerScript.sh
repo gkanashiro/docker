6eb505588dd1ff1587d4b94b7774017fb38387e5
Gabriel Kanashiro <gabksouza@outlook.com.br>
2022-10-13 20:21:40 -0300

    Script docker

---
+++ dockerScript.sh
@@ -0,0 +1,12 @@
+#apt-get install -y  docker.io
+#instala do repositorio ubuntu o docker.io.
+
+docker -v
+#verifica a versão do docker.
+
+docker version
+#mostra mais detalhes do client e service.
+
+docker info
+#retorna mais informacoes sobre o docker.
+#verifique se você tem as permissoes necessarias para executar este comando
